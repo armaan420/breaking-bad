32e4e171d836866b69ba773b23de595ea9864a3a
armaan singh <armaanmaani@gmail.com>
2021-02-01 08:52:06 -0500

    Filter method implemented

--- src/App.js
+++ src/App.js
@@ -8,23 +8,26 @@ import './App.css';
 const App = () => {
   const [items, setItems] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     const fetchItems = async () => {
-      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
-      console.log(result.data)
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
+      );
+      console.log(result.data);
 
-      setItems(result.data)
-      setIsLoading(false)
-    }
+      setItems(result.data);
+      setIsLoading(false);
+    };
 
-    fetchItems()
-  }, [])
+    fetchItems();
+  }, [query]);
 
   return (
     <div className="container">
       <Header />
-      <Search />
+      <Search getQuery={(q) => setQuery(q)} />
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
